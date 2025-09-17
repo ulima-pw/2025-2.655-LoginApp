b0412326e83ffcc46816d3e65f42151030369ec7
ProfI2104 <ProfI2104@lab.aca>
2025-09-17 12:30:37 -0500

    Agregando bootstrap y estilos a componente react

--- src/SeccionCentral.tsx
+++ src/SeccionCentral.tsx
@@ -1,9 +1,14 @@
 import FormularioLogin from "./FormularioLogin"
+import Titulo from "./Titulo"
+
+import "./SeccionCentral.css"
 
 const SeccionCentral = () => {
     return <div>
         { /* este es un comentario */ }
-        <img src='https://upload.wikimedia.org/wikipedia/commons/a/a0/Universidad_de_Lima_logo.png' />
+        <Titulo />
+        <img className="logo" 
+            src='https://upload.wikimedia.org/wikipedia/commons/a/a0/Universidad_de_Lima_logo.png' />
         <FormularioLogin />
     </div>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import LoginPage from './LoginPage'
 
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 //dffds
 createRoot(document.getElementById('root')!).render(
