e2b8229dc967ef5592bada9c4eaac646d76a881e
ProfI2104 <ProfI2104@lab.aca>
2025-09-22 12:26:27 -0500

    Formularios gestionados

--- src/FormularioLogin.tsx
+++ src/FormularioLogin.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react"
+
 const FormularioLogin = () => {
+    const [ username, setUsername ] = useState<string>("")
+    const [ password, setPassword ] = useState<string>("")
+
+    const handleUsernameOnChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.currentTarget.value)
+    }
+
+    const handlePasswordOnChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }
+
     return <form>
         <div>
             <label className="form-label">Usuario:</label>
-            <input className="form-control" type='text' />
+            <input className="form-control" 
+                type='text'
+                value={ username }
+                onChange={ handleUsernameOnChange } />
         </div>
         <div>
             <label className="form-label">Password:</label>
-            <input className="form-control" type='password' />
+            <input className="form-control" 
+                type='password'
+                value={ password }
+                onChange={ handlePasswordOnChange } />
         </div>
         <button className="btn btn-success w-100 mt-3" 
             type='button'>
