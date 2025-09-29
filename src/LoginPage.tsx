626314693c1ac9760938e40539563b6d14ca5e41
ProfI2104 <ProfI2104@lab.aca>
2025-09-29 12:12:39 -0500

    Login con sessionStorage

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -13,10 +13,40 @@ const LoginPage = () => {
         if (username == "PW" && password == "billy") {
             // Login correcto
             setHuboErrorLogin(false)
+            const usuarioAGuardar = {
+                username : username
+            }
+            sessionStorage.setItem(
+                "USUARIO", 
+                JSON.stringify(usuarioAGuardar)
+            )
+
             navigate("/main")
         }else {
             // Login incorrecto
             setHuboErrorLogin(true)
+
+            const usuario = sessionStorage.getItem("USUARIO")
+            if (usuario == null) {
+                const usuarioAGuardar = {
+                    username : username,
+                    contadorClicks : 1
+                }
+                sessionStorage.setItem(
+                    "USUARIO", 
+                    JSON.stringify(usuarioAGuardar)
+                )
+            }else {
+                const usuarioTS = JSON.parse(usuario)
+                const usuarioAGuardar = {
+                    username : username,
+                    contadorClicks : usuarioTS.contadorClicks + 1
+                }
+                sessionStorage.setItem(
+                    "USUARIO", 
+                    JSON.stringify(usuarioAGuardar)
+                )
+            }
         }
     }
 
--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom"
 import Titulo from "./Titulo"
 
 const MainPage = () => {
+    const navigate = useNavigate()
+    
+    const logout = () => {
+        sessionStorage.clear()
+        navigate("/")
+    }
+
     return <div>
         <Titulo texto={"Main"}/>
+        <button className="btn btn-danger" 
+            onClick={ logout }>
+                Logout
+        </button>
     </div>
 }
 
